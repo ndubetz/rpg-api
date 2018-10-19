7152db490f19e6fff5019f03fa0d3f69db7d3b73
NDUB <ndubetz@pillartechnology.com>
2018-10-18 23:31:31 -0400

    Initial commit

---
+++ src/main/java/com/fntzy/RPG/API/RpgApiApplication.java
@@ -0,0 +1,12 @@
+package com.fntzy.RPG.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgApiApplication.class, args);
+	}
+}
