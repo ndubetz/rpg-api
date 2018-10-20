da245bd67b37bacd7344d1703c6ce1c26a549efd
NDUB <ndubetz@pillartechnology.com>
2018-10-19 21:15:21 -0400

    Bump attributes per rank

--- src/main/java/com/fntzy/core/model/RankAttributeMaps.java
+++ src/main/java/com/fntzy/core/model/RankAttributeMaps.java
@@ -10,35 +10,35 @@ public class RankAttributeMaps {
     private static final Map<Integer, Integer> tertiaryRankToAttributeMap = new HashMap<>();
 
     static {
-        primaryRankToAttributeMap.put(1, 1);
-        primaryRankToAttributeMap.put(2, 2);
-        primaryRankToAttributeMap.put(3, 3);
-        primaryRankToAttributeMap.put(4, 4);
-        primaryRankToAttributeMap.put(5, 6);
-        primaryRankToAttributeMap.put(6, 7);
-        primaryRankToAttributeMap.put(7, 8);
-        primaryRankToAttributeMap.put(8, 10);
-        primaryRankToAttributeMap.put(9, 13);
+        primaryRankToAttributeMap.put(1, 2);
+        primaryRankToAttributeMap.put(2, 3);
+        primaryRankToAttributeMap.put(3, 4);
+        primaryRankToAttributeMap.put(4, 6);
+        primaryRankToAttributeMap.put(5, 8);
+        primaryRankToAttributeMap.put(6, 10);
+        primaryRankToAttributeMap.put(7, 11);
+        primaryRankToAttributeMap.put(8, 13);
+        primaryRankToAttributeMap.put(9, 16);
 
         secondaryRankToAttributeMap.put(1, 0);
-        secondaryRankToAttributeMap.put(2, 0);
-        secondaryRankToAttributeMap.put(3, 1);
-        secondaryRankToAttributeMap.put(4, 2);
-        secondaryRankToAttributeMap.put(5, 3);
-        secondaryRankToAttributeMap.put(6, 4);
-        secondaryRankToAttributeMap.put(7, 6);
-        secondaryRankToAttributeMap.put(8, 8);
-        secondaryRankToAttributeMap.put(9, 10);
+        secondaryRankToAttributeMap.put(2, 1);
+        secondaryRankToAttributeMap.put(3, 2);
+        secondaryRankToAttributeMap.put(4, 3);
+        secondaryRankToAttributeMap.put(5, 5);
+        secondaryRankToAttributeMap.put(6, 6);
+        secondaryRankToAttributeMap.put(7, 8);
+        secondaryRankToAttributeMap.put(8, 10);
+        secondaryRankToAttributeMap.put(9, 12);
 
         tertiaryRankToAttributeMap.put(1, 0);
         tertiaryRankToAttributeMap.put(2, 0);
         tertiaryRankToAttributeMap.put(3, 0);
         tertiaryRankToAttributeMap.put(4, 0);
         tertiaryRankToAttributeMap.put(5, 0);
-        tertiaryRankToAttributeMap.put(6, 1);
-        tertiaryRankToAttributeMap.put(7, 2);
-        tertiaryRankToAttributeMap.put(8, 4);
-        tertiaryRankToAttributeMap.put(9, 7);
+        tertiaryRankToAttributeMap.put(6, 2);
+        tertiaryRankToAttributeMap.put(7, 4);
+        tertiaryRankToAttributeMap.put(8, 6);
+        tertiaryRankToAttributeMap.put(9, 8);
     }
 
     public static Map<Integer, Integer> primaryRankToAttributeMap() {
